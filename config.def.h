b5449782b2b5599457df1d284f60835ed4d1970f
duncy <fryerduncan@gmail.com>
2026-08-30 11:23:03 +1200

    patched barpadding manually merged to work with tabs runs

--- config.def.h
+++ config.def.h
@@ -8,6 +8,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int swterminheritfs    = 1;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int showtab            = 1;        /* 0 means no tabbar */
 static const int alltab             = 0;        /* 0 means no tabbar for all layouts */
 static const int toptab             = 0;        /* 1 means top tab bar */
@@ -120,7 +122,6 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
